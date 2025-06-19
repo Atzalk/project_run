d41e4510126269f2b54b76a33a51eeea9c9ffbe0
Nikolay <chertkov.kolyan@gmail.com>
2025-06-19 13:00:57 +0300

    models Run

--- app_run/admin.py
+++ app_run/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Run
+
 
 # Register your models here.
+@admin.register(Run)
+class RunAdmin(admin.ModelAdmin):
+    pass
--- app_run/views.py
+++ app_run/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import viewsets
+from .models import Run
+from .serializers import RunSerializer
 
 
 @api_view(['GET'])
@@ -14,3 +17,8 @@ def my_function_based_view(request):
         'slogan': 'Не бегаем в любую погоду! От -30 до +30!',
         'contacts': 'Город Москва, Олимпийская деревня'
     })
+
+
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
