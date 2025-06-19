d4453b56260488604dbdb224ddcbc38647e48bba
Nikolay <chertkov.kolyan@gmail.com>
2025-06-19 13:12:38 +0300

    models Run

--- app_run/models.py
+++ app_run/models.py
@@ -6,5 +6,5 @@ from django.contrib.auth.models import User
 
 class Run(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    athlete = models.ForeignKey(User, on_delete=models.PROTECT)
-    comment = models.TextField(blank=True)
+    athlete = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
