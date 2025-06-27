68c0c480aaf49c097b72b97e96ed6ac2e06e41e7
Nikolay <chertkov.kolyan@gmail.com>
2025-06-27 10:49:30 +0300

    count runs finished

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -10,13 +10,17 @@ class RunSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     type = serializers.SerializerMethodField()
+    runs_finished = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ['id', 'date_joined', 'username', 'last_name', 'first_name', 'type']
+        fields = ['id', 'date_joined', 'username', 'last_name', 'first_name', 'type', 'runs_finished']
 
     def get_type(self, obj):
         return 'coach' if obj.is_staff else 'athlete'
+
+    def get_runs_finished(self, obj):
+        return len(Run.objects.filter(athlete__id=obj.id, status='finished'))
 
 
 class UserSerializerForRun(serializers.ModelSerializer):
